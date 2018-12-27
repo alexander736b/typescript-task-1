28a346618086ceed8f37f6a747b6ae3477d30d41
Alexander Kartashov <alexander736b@yandex.ru>
2018-12-27 21:28:03 +0500

    Убрал лишнее

--- src/views/desktop.ts
+++ src/views/desktop.ts
@@ -1,7 +1,7 @@
 import { View } from './index';
 
 export class DesktopView extends View {
-    protected nameClass: string = 'desktop';
-    protected recordNumberNews: number = 3;
-    protected recordNumberWeather: number = 2;
+    protected nameClass = 'desktop';
+    protected recordNumberNews = 3;
+    protected recordNumberWeather = 2;
 }
--- src/views/mobile.ts
+++ src/views/mobile.ts
@@ -1,7 +1,7 @@
 import { View } from './index';
 
 export class MobileView extends View {
-    protected nameClass: string = 'mobile';
-    protected recordNumberNews: number = 1;
-    protected recordNumberWeather: number = 1;
+    protected nameClass = 'mobile';
+    protected recordNumberNews = 1;
+    protected recordNumberWeather = 1;
 }
